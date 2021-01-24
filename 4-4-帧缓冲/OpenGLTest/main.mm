856d32f5423d066761543d99f7945f3051801bb1
Uber <youbo@xiandanjia.com>
2021-01-24 23:10:49 +0800

    帧缓冲

--- 4-4-帧缓冲/OpenGLTest/main.mm
+++ 4-4-帧缓冲/OpenGLTest/main.mm
@@ -118,7 +118,7 @@ unsigned int getTexture() {
     glGenTextures(1,&texture);
     glBindTexture(GL_TEXTURE_2D,texture);
     
-    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,800,600,0,GL_RGB,GL_UNSIGNED_BYTE,NULL);
+    glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,SCR_WIDTH,SCR_HEIGHT,0,GL_RGB,GL_UNSIGNED_BYTE,NULL);
     
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
@@ -137,7 +137,7 @@ unsigned int getBuffer(unsigned int texture) {
     unsigned int rbo;
     glGenRenderbuffers(1,&rbo);
     glBindRenderbuffer(GL_RENDERBUFFER,rbo);
-    glRenderbufferStorage(GL_RENDERBUFFER,GL_DEPTH24_STENCIL8,800,600);
+    glRenderbufferStorage(GL_RENDERBUFFER,GL_DEPTH24_STENCIL8,SCR_WIDTH,SCR_HEIGHT);
     glBindRenderbuffer(GL_RENDERBUFFER,0);
     glFramebufferRenderbuffer(GL_FRAMEBUFFER,GL_DEPTH_STENCIL_ATTACHMENT,GL_RENDERBUFFER,rbo);
     
