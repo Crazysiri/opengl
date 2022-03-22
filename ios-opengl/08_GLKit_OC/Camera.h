10f952dfa708869a27993ab7109e5ba44e576ffd
Uber <youbo@xiandanjia.com>
2022-03-22 08:11:15 +0800

    camera

--- ios-opengl/08_GLKit_OC/Camera.h
+++ ios-opengl/08_GLKit_OC/Camera.h
@@ -32,6 +32,9 @@ const float SPEED = 2.5f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 45.0f;
 
+vec3 zero = {0.0,0.0,0.0};
+vec3 identity = {0.0,1.0,0.0};
+
 class Camera {
 public:
     vec3 Position;
@@ -46,10 +49,9 @@ public:
     float MovementSpeed;
     float MouseSensitivity;
     float Zoom;
-    
-    Camera(glm::vec3 position = glm_vec3(0.0f,0.0f,0.0f),glm::vec3 up = glm::vec3(0.0f,1.0f,0.0f),float yaw = YAW,float pitch = PITCH) : Front(glm::vec3(0.0f,0.0f,-1.0f)),MovementSpeed(SPEED),MouseSensitivity(SENSITIVITY),Zoom(ZOOM) {
-        Position = position;
-        WorldUp = up;
+    Camera(vec3 position = zero,vec3 up = identity,float yaw = YAW,float pitch = PITCH) : Front{0.0f,0.0f,-1.0f},MovementSpeed(SPEED),MouseSensitivity(SENSITIVITY),Zoom(ZOOM) {
+        glm_vec3_copy(position, Position);
+        glm_vec3_copy(up, WorldUp);
         Yaw = yaw;
         Pitch = pitch;
         
@@ -57,16 +59,18 @@ public:
     }
     
     
-    Camera(float posX,float posY,float posZ,float upX,float upY,float upZ,float yaw,float pitch): Front(glm::vec3(0.0f,0.0f,-1.0f)),MovementSpeed(SPEED),MouseSensitivity(SENSITIVITY),Zoom(ZOOM) {
-        Position = glm::vec3(posX,posY,posZ);
-        WorldUp = glm::vec3(upX,upY,upZ);
+    Camera(float posX,float posY,float posZ,float upX,float upY,float upZ,float yaw,float pitch): Front{0.0f,0.0f,-1.0f},MovementSpeed(SPEED),MouseSensitivity(SENSITIVITY),Zoom(ZOOM) {
+        vec3 p = {posX,posY,posZ};
+        glm_vec3_copy(p, Position);
+        vec3 up = {upX,upY,upZ};
+        glm_vec3_copy(up, WorldUp);
         Yaw = yaw;
         Pitch = pitch;
         updateCameraVectors();
 
     }
     
-    glm::mat4 GetViewMatrix() {
+    void GetViewMatrix(mat4 dst) {
         //looktAt参数：摄像机位置P 目标位置 世界空间的上向量
         //摄像机位置和目标位置 可以算出 摄像机方向D （这个方向和摄像机对着的方向是相反的）
         //上向量 和 摄像机方向 叉乘可以算出 右轴R
@@ -82,26 +86,29 @@ public:
         //https://learnopengl-cn.github.io/01%20Getting%20started/09%20Camera/
         
 //        return lookAt(Position, Position + Front, Up);
-        return glm::lookAt(Position, Position + Front, Up);
+        vec3 d;
+        glm_vec3_copy(zero, d);
+        glm_vec3_add(Position, Front, d);
+        glm_lookat(Position, d, Up, dst);
     }
     
-    void ProcessKeyboard(Camera_Movement direction,float deltaTime) {
-        float velocity = MovementSpeed * deltaTime;
-        if (direction == FORWARD) {
-            Position += Front * velocity;
-        }
-        if (direction == BACKWARD) {
-            Position -= Front * velocity;
-        }
-        if (direction == LEFT) {
-            Position -= Right * velocity;
-        }
-        if (direction == RIGHT) {
-            Position += Right * velocity;
-        }
-        
-        Position.y = 0.0f;
-    }
+//    void ProcessKeyboard(Camera_Movement direction,float deltaTime) {
+//        float velocity = MovementSpeed * deltaTime;
+//        if (direction == FORWARD) {
+//            Position += Front * velocity;
+//        }
+//        if (direction == BACKWARD) {
+//            Position -= Front * velocity;
+//        }
+//        if (direction == LEFT) {
+//            Position -= Right * velocity;
+//        }
+//        if (direction == RIGHT) {
+//            Position += Right * velocity;
+//        }
+//
+//        Position.y = 0.0f;
+//    }
     
     void ProcessMouseMovement(float xoffset,float yoffset,GLboolean constrainPitch = true) {
         xoffset *= MouseSensitivity;
@@ -134,44 +141,53 @@ public:
     
 private:
     void updateCameraVectors() {
-        glm::vec3 front;
-        front.x = cos(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-        front.y = sin(glm::radians(Pitch));
-        front.z = sin(glm::radians(Yaw)) * cos(glm::radians(Pitch));
-        Front = glm::normalize(front);
+        vec3 front;
+        front[0] = cos(glm_rad(Yaw)) * cos(glm_rad(Pitch));
+        front[1] = sin(glm_rad(Pitch));
+        front[2] = sin(glm_rad(Yaw)) * cos(glm_rad(Pitch));
+        glm_normalize(front);
+        glm_vec3_copy(front, Front);
         
-        Right = glm::normalize(glm::cross(Front, WorldUp));
-        Up = glm::normalize(glm::cross(Front, Right));
+        
+        vec3 r;
+        glm_cross(Front, WorldUp, r);
+        glm_normalize(r);
+        glm_vec3_copy(r, Right);
+
+        vec3 u;
+        glm_cross(Front, Right, u);
+        glm_normalize(u);
+        glm_vec3_copy(u, Up);
     }
     
-    glm::mat4 lookAt(glm::vec3 position,glm::vec3 target,glm::vec3 worldUp) {
-       glm::vec3 right(1.0);
-       glm::vec3 up(1.0);
-       glm::vec3 direction(1.0);
-       direction = glm::normalize(position - target);
-       right = glm::normalize(glm::cross(direction, WorldUp));
-       up = glm::cross(direction, right);
-       glm::mat4 rotation(1.0f);
-       rotation[0][0] = right.x;
-       rotation[1][0] = right.y;
-       rotation[2][0] = right.z;
-
-       rotation[0][1] = up.x;
-       rotation[1][1] = up.y;
-       rotation[2][1] = up.z;
-       
-       rotation[0][2] = direction.x;
-       rotation[1][2] = direction.y;
-       rotation[2][2] = direction.z;
-       
-       
-       glm::mat4 translation(1.0f);
-       translation[3][0] = -position.x;
-       translation[3][1] = -position.y;
-       translation[3][2] = -position.z;
-       
-       return rotation * translation;
-    }
+//    lookAt(glm::vec3 position,glm::vec3 target,glm::vec3 worldUp, mat4 dst) {
+//       glm::vec3 right(1.0);
+//       glm::vec3 up(1.0);
+//       glm::vec3 direction(1.0);
+//       direction = glm::normalize(position - target);
+//       right = glm::normalize(glm::cross(direction, WorldUp));
+//       up = glm::cross(direction, right);
+//       glm::mat4 rotation(1.0f);
+//       rotation[0][0] = right.x;
+//       rotation[1][0] = right.y;
+//       rotation[2][0] = right.z;
+//
+//       rotation[0][1] = up.x;
+//       rotation[1][1] = up.y;
+//       rotation[2][1] = up.z;
+//       
+//       rotation[0][2] = direction.x;
+//       rotation[1][2] = direction.y;
+//       rotation[2][2] = direction.z;
+//       
+//       
+//       glm::mat4 translation(1.0f);
+//       translation[3][0] = -position.x;
+//       translation[3][1] = -position.y;
+//       translation[3][2] = -position.z;
+//       
+//       return rotation * translation;
+//    }
 };
 
 #endif /* Camera_h */
